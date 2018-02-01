df6ddf152a08b4282311ce02cfb3deb8ef0f2970
Bartosz Lampart <dzazder@gmail.com>
2018-02-01 19:17:35 +0100

    fragment layout

--- app/src/main/java/com/blsoft/homecycling/MainActivity.java
+++ app/src/main/java/com/blsoft/homecycling/MainActivity.java
@@ -1,13 +1,49 @@
 package com.blsoft.homecycling;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.blsoft.homecycling.dicts.DictFragment;
+import com.blsoft.homecycling.fragments.HomeFragment;
+import com.blsoft.homecycling.global.ConstStrings;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            switchFragment(DictFragment.HOME);
+        }
+
+        fragmentManager = getFragmentManager();
+    }
+
+    /**
+     * It runs selected fragment
+     * @param fragment Id of fragment to run, ids are saved in MainActivity fields
+     */
+    public void switchFragment(DictFragment fragment) {
+        Fragment newFragment = null;
+        switch (fragment) {
+            case HOME:
+                newFragment = new HomeFragment();
+                break;
+        }
+        if (!isFinishing()) {
+            FragmentTransaction transaction = getFragmentManager().beginTransaction();
+
+            transaction.replace(R.id.layout_fragment_area, newFragment, ConstStrings.CURRENT_FRAGMENT);
+            transaction.addToBackStack(null);
+
+            transaction.commitAllowingStateLoss();
+        }
     }
 }
---
+++ app/src/main/java/com/blsoft/homecycling/global/ConstStrings.java
@@ -0,0 +1,9 @@
+package com.blsoft.homecycling.global;
+
+/**
+ * Created by bartek on 01.02.2018.
+ */
+
+public class ConstStrings {
+    public static String CURRENT_FRAGMENT = "FRAGMENT_DISPLAYED";
+}
